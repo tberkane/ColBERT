301f840e61e304dd8cb3fe46337c1c7908dcf1f6
thomas <thomas.berkane@epfl.ch>
2024-06-12 10:20:48 +0900

    add prints

--- colbert/utils/distributed.py
+++ colbert/utils/distributed.py
@@ -11,7 +11,7 @@ ALREADY_INITALIZED = False
 
 
 def init(rank):
-    os.environ['NCCL_BLOCKING_WAIT'] = '0'  # not to enforce timeout
+    # os.environ['NCCL_BLOCKING_WAIT'] = '0'  # not to enforce timeout
     nranks = 'WORLD_SIZE' in os.environ and int(os.environ['WORLD_SIZE'])
     nranks = max(1, nranks)
     is_distributed = (nranks > 1) or ('WORLD_SIZE' in os.environ)
@@ -27,11 +27,11 @@ def init(rank):
         print(f'nranks = {nranks} \t num_gpus = {num_gpus} \t device={rank % num_gpus}')
 
         torch.cuda.set_device(rank % num_gpus)
-        # torch.distributed.init_process_group(backend='nccl', init_method='env://')
-        dist.init_process_group('nccl' if dist.is_nccl_available() else 'gloo', init_method='env://',
-                                timeout=timedelta(seconds=7200000),  # was 1800000
-                                rank=nranks,
-                                world_size=int(os.environ['WORLD_SIZE']))
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+        # dist.init_process_group('nccl' if dist.is_nccl_available() else 'gloo', init_method='env://',
+        #                         timeout=timedelta(seconds=7200000),  # was 1800000
+        #                         rank=nranks,
+        #                         world_size=int(os.environ['WORLD_SIZE']))
 
     return nranks, is_distributed
 
